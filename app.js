8c99e4dbb19756cb3ae2d9ffe5d8e89a730978e2
Vearso <gus.szymon@gmail.com>
2020-09-18 20:11:27 +0200

    Small changes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const button = document.querySelector(".page__nav--burger");
 const menu = document.querySelector(".page__nav--list");
+const button2 = document.querySelector(".page__nav--cross");
 
 button.addEventListener("click", function (){
     menu.classList.toggle("show");
     button.classList.toggle("show");
+    button2.classList.toggle("show");
 })
